18157ce11dfce6230711da9ad886d9b62487bf61
Eduardo Isidoro Gonçalves <eduardoi@fuvs.br>
2021-05-14 09:30:08 -0300

    Adicionado icones e botoes

--- lib/main.dart
+++ lib/main.dart
@@ -30,6 +30,34 @@ class _HomeState extends State<Home> {
           )
         ],
       ),
+      backgroundColor: Colors.white, // Adionando cor do fundo geral do app.
+      // padronizando o body como coluna, ou seja, nossa estrutura será um coluna vertical.s
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment
+            .stretch, // Posicionando no centro o eixo cruzado.
+        children: <Widget>[
+          // Adicionado um widge do tipo Icon.
+          Icon(
+            Icons.account_box_rounded,
+            size: 120,
+            color: Colors.purple,
+          ),
+          // adicionado um widget do tipo textField, um campo de digitação que recebe valor númerico.
+          TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                  labelText: "Peso (kg)",
+                  labelStyle: TextStyle(color: Colors.purple, fontSize: 30.0))),
+          TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                  labelText: "Altura (cm)",
+                  labelStyle: TextStyle(
+                    color: Colors.purple,
+                    fontSize: 30.0,
+                  )))
+        ],
+      ),
     );
   }
 }
